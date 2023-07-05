10f692950cd61b03a5deaf500e7072b8620d05f7
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-07-04 23:56:46 -0400

    Cartoons Collected

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,24 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    num = 1
+    for dwarf in dwarves:
+        print(f'{num}. {dwarf}')
+        num+=1
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(elements):
+    return [f'{element.capitalize()}!' for element in elements ]
+    
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    return any([call for call in calls if len(call)>4])
+    
+
+def find_the_cheese(foods):
+    types_of_cheese = ["cheddar", "gouda", "camembert"]
+    for food in foods:
+        if food in types_of_cheese: 
+            return food
+    return None
+
+soup = ["tomato soup", "oyster crackers"]
+print(find_the_cheese(soup))
